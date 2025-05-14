ac9325aab55c2569ba538d68f58dc5fa33b41e6b
taufeeqsyed62 <136118536+taufeeqsyed62@users.noreply.github.com>
2025-05-15 02:18:32 +0530

    s

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -41,7 +41,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Crypto Hunter
+            Coin Biz
           </Typography>
           <Typography
             variant="subtitle2"
